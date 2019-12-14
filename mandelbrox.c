ca15bd0cb73d894b7f72b2740271deeb92f7534b
Paul DeLong <Paul.DeLong@gmail.com>
2019-12-14 00:05:59 -0500

    added core loop, with some bounds to test with;

--- mandelbrox.c
+++ mandelbrox.c
@@ -1,2 +1,18 @@
+#include <stdio.h>
+
 int main( int argc, char *argv[] ) {
+
+  int width = 1000;
+  int height = 1000;
+  int max_iter = 1000;
+  double x_min = -2.0;
+  double x_max = +2.0;
+  double y_min = -2.0;
+  double y_max = +2.0;
+
+  for( int j = 0; j < height; ++j ) {
+    for( int i = 0; i < width; ++i ) {
+      printf( "(%d, %d)\n", i, j );
+    }
+  }
 }
