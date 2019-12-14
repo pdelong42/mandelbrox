8c8ab70599148031c4a79d5acdec7e4dced00816
Paul DeLong <Paul.DeLong@gmail.com>
2019-12-14 00:52:15 -0500

    got a basic test working in text-only mode;

--- mandelbrox.c
+++ mandelbrox.c
@@ -3,13 +3,10 @@
 int main( int argc, char *argv[] ) {
 
   int width = 100;
-  int height = 100;
+  int height = 50;
   int max_iter = 1000;
-  double x_min = -2.0;
-  double x_max = +2.0;
-  double y_min = -2.0;
-  double y_max = +2.0;
-
+  double x_min = -2.0, x_max = +2.0;
+  double y_min = -2.0, y_max = +2.0;
   double x_ratio = ( x_max - x_min ) / width;
   double y_ratio = ( y_max - y_min ) / height;
 
@@ -17,10 +14,27 @@ int main( int argc, char *argv[] ) {
 
     for( int i = 0; i < width; ++i ) {
 
-      double x = x_min + i * x_ratio;
-      double y = y_min + j * y_ratio;
+      int iter = 0;
+      double x = 0.0, xx = 0.0;
+      double y = 0.0, yy = 0.0;
+      double a = x_min + i * x_ratio;
+      double b = y_min + j * y_ratio;
 
-      printf( "(%f, %f)\n", x, y );
+      while( ++iter < max_iter ) {
+
+        double xx = x * x;
+        double yy = y * y;
+
+        if( xx + yy > 4 ) break;
+
+        double x_new = a + xx - yy;
+        double y_new = b + 2 * x * y;
+
+        x = x_new, y = y_new;
+      }
+
+      printf( ( iter < max_iter ) ? "X" : " " );
     }
+    printf( "\n" );
   }
 }
