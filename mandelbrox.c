9e16f1fa49bbdc87450c316e9d4b531d675139cf
Paul DeLong <Paul.DeLong@gmail.com>
2019-12-14 16:08:41 -0500

    switched output format to PBM (ASCII);

--- mandelbrox.c
+++ mandelbrox.c
@@ -2,13 +2,22 @@
 
 int main( int argc, char *argv[] ) {
 
-  int width = 100;
-  int height = 50;
+  //int width = 100;
+  //int height = 50;
+  int width  = 4096;
+  int height = 4096;
   int max_iter = 1000;
   double x_min = -2.0, x_max = +2.0;
   double y_min = -2.0, y_max = +2.0;
   double x_ratio = ( x_max - x_min ) / width;
   double y_ratio = ( y_max - y_min ) / height;
+
+  printf( "P1\n%d %d\n", width, height );
+  printf( "# x_min = %f\n", x_min );
+  printf( "# x_max = %f\n", x_max );
+  printf( "# y_min = %f\n", y_min );
+  printf( "# y_max = %f\n", y_max );
+  printf( "# max_iter = %d\n", max_iter );
 
   for( int j = 0; j < height; ++j ) {
 
@@ -33,7 +42,7 @@ int main( int argc, char *argv[] ) {
         x = x_new, y = y_new;
       }
 
-      printf( ( iter < max_iter ) ? "X" : " " );
+      printf( ( iter < max_iter ) ? "0 " : "1 " );
     }
     printf( "\n" );
   }
