f519edc824287dbf40ce423cae87a89b10d1acae
Paul DeLong <Paul.DeLong@gmail.com>
2019-12-21 12:53:41 -0500

    [feature/getopt] minimal getopt() functionality;
    I created the minimal skeleton of the logic necessary for starting to parse
    command-line arguments; just the variable declarations and the main loop.
    
    It's basically just a big null-op, if there are no args passed in, and it
    doesn't break anything.

--- mandelbrox.c
+++ mandelbrox.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <getopt.h>
 
 int main( int argc, char *argv[] ) {
 
@@ -10,6 +11,46 @@ int main( int argc, char *argv[] ) {
   double y_min = -2.0, y_max = +2.0;
   double x_ratio = ( x_max - x_min ) / width;
   double y_ratio = ( y_max - y_min ) / height;
+
+  static struct option long_options[] = {
+    {"format", required_argument, 0, 'f' },
+    {"verbose", no_argument,      0, 'v' },
+    {"width",   no_argument,      0, 'w' },
+    {"height",  no_argument,      0, 'h' },
+    {0,         0,                0,   0 }
+  };
+
+
+  while( 1 ) {
+
+    int this_option_optind = optind ? optind : 1;
+    int digit_optind = 0;
+    int option_index = 0;
+
+    int c = getopt_long( argc, argv, "abc:d:", long_options, &option_index );
+
+    if( c < 0 ) break;
+
+    switch (c) {
+    case 0:
+      printf( "option %s", long_options[option_index].name );
+      if( optarg ) printf(" with arg %s", optarg);
+      printf( "\n" );
+      break;
+
+    case '?':
+      break;
+
+    default:
+      printf( "?? getopt returned character code 0%o ??\n", c );
+    }
+  }
+
+  if( optind < argc ) {
+    fprintf( stderr, "non-option ARGV-elements: " );
+    while( optind < argc ) fprintf( stderr, "%s ", argv[optind++] );
+    fprintf( stderr, "\n");
+  }
 
   printf( "P1\n%d %d\n", width, height );
   printf( "# x_min = %f\n", x_min );
