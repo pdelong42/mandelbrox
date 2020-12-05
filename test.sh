087e778dba154cfc3f846b6ca58a5c362d56e2c6
Paul DeLong <Paul.DeLong@gmail.com>
2020-12-05 17:55:17 -0500

    making my test script more useful;

--- test.sh
+++ test.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+# This test uses a patch of the complex plane that is contained
+# entirely within the Mandelbrot set, such that every point takes the
+# same amount of time.
+
 set -xeuo pipefail
 
+OPTARGS="-t 4 -M 1000000 -w 100 -h 100"
+BASEARGS="-x -0.5 -X 0 -y -0.5 -Y 0"
+
+if test -n "$*" ; then
+   ARGS="$BASEARGS $*"
+else
+   ARGS="$BASEARGS $OPTARGS"
+fi
+
 rm -vf a.ppm b.ppm c.ppm
-/usr/bin/time ./mandelbrox -B plain          -t 4 > a.ppm
-/usr/bin/time ./mandelbrox -B threads_simple -t 4 > b.ppm
-/usr/bin/time ./mandelbrox -B threads_naive  -t 4 > c.ppm
+
+/usr/bin/time ./mandelbrox -B plain          $ARGS > a.ppm
+/usr/bin/time ./mandelbrox -B threads_simple $ARGS > b.ppm
+/usr/bin/time ./mandelbrox -B threads_naive  $ARGS > c.ppm
