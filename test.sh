e854fcdf7f06a3131cd208e79e5c1c9fad3828dd
Paul DeLong <Paul.DeLong@gmail.com>
2020-12-05 22:06:04 -0500

    minor improvement to test script;

--- test.sh
+++ test.sh
@@ -6,8 +6,9 @@
 
 set -xeuo pipefail
 
-OPTARGS="-t 4 -M 1000000 -w 100 -h 100"
-BASEARGS="-x -0.5 -X 0 -y -0.5 -Y 0"
+CMD="/usr/bin/time ./mandelbrox"
+BASEARGS="-x -0.5 -X 0 -y -0.5 -Y 0 -t 4"
+OPTARGS="-M 1000000 -w 100 -h 100"
 
 if test -n "$*" ; then
    ARGS="$BASEARGS $*"
@@ -17,6 +18,6 @@ fi
 
 rm -vf a.ppm b.ppm c.ppm
 
-/usr/bin/time ./mandelbrox -B plain          $ARGS > a.ppm
-/usr/bin/time ./mandelbrox -B threads_simple $ARGS > b.ppm
-/usr/bin/time ./mandelbrox -B threads_naive  $ARGS > c.ppm
+$CMD -B plain          $ARGS > a.ppm
+$CMD -B threads_simple $ARGS > b.ppm
+$CMD -B threads_naive  $ARGS > c.ppm
