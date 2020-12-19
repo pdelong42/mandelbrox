0f4cd6c46466fc5499b13adea8bae504bb37a36c
Paul DeLong <Paul.DeLong@gmail.com>
2020-12-18 22:39:49 -0500

    closer to what I want, but still somehwat buggy;

--- scripts/frames.pl
+++ scripts/frames.pl
@@ -18,12 +18,17 @@ Ultimately, I think I'll probably integrate this feature into the C
 codebase of the main program.  But for now, I just wanted a POC in a
 langauge I know I could bang it out in relatively quickly.
 
-Example: ./frames.pl 1000 0.425 0.45 0.355 0.380 -2 2 -2 2
+Example: ./frames.pl 100 -2 2 -2 2 0.425 0.45 0.355 0.380 
 
 =cut
 
 use strict;
 use warnings;
+
+my $frame = 0;
+
+my $x_scale = 1;
+my $y_scale = 1;
 
 scalar( @ARGV ) == 9
    or die "nine args required - aborting\n";
@@ -32,27 +37,67 @@ my( $frames, $x1_min, $x1_max, $y1_min, $y1_max, $x2_min, $x2_max, $y2_min, $y2_
 
 sub nth_root { exp( log( shift() ) / $frames ) }
 
-my $x1_mid = ( $x1_max + $x1_min ) / 2;
-my $y1_mid = ( $y1_max + $y1_min ) / 2;
-my $x2_mid = ( $x2_max + $x2_min ) / 2;
-my $y2_mid = ( $y2_max + $y2_min ) / 2;
+my $x1_span = $x1_max - $x1_min;
+my $y1_span = $y1_max - $y1_min;
+my $x2_span = $x2_max - $x2_min;
+my $y2_span = $y2_max - $y2_min;
 
-my $x1_rad = ( $x1_max - $x1_min ) / 2;
-my $y1_rad = ( $y1_max - $y1_min ) / 2;
-my $x2_rad = ( $x2_max - $x2_min ) / 2;
-my $y2_rad = ( $y2_max - $y2_min ) / 2;
+my $x_ratio = $x1_span / $x2_span;
+my $y_ratio = $y1_span / $y2_span;
 
-my $x_ratio = $x1_rad / $x2_rad;
-my $y_ratio = $y1_rad / $y2_rad;
+my $x_pop = nth_root $x_ratio;
+my $y_pop = nth_root $y_ratio;
 
-my $x_step = nth_root $x_ratio;
-my $y_step = nth_root $y_ratio;
+my $x_max_delta = $x1_max - $x2_max;
+my $y_max_delta = $y1_max - $y2_max;
 
-printf "x_ratio = %lf\n", $x_ratio;
-printf "y_ratio = %lf\n", $y_ratio;
+my $x_min_delta = $x1_min - $x2_min;
+my $y_min_delta = $y1_min - $y2_min;
 
-printf "x_step = %lf\n", $x_step;
-printf "y_step = %lf\n", $y_step;
+my( $x_min, $x_max, $y_min, $y_max ) = ( $x2_min, $x2_max, $y2_min, $y2_max );
 
-# Obviously, this isn't finished yet.  The only output I'm producing
-# so-far is just for intermediate sanity-checking purposes.
+print <<'EOF';
+#!/bin/bash
+
+set -euo pipefail
+
+SELF=$(realpath $0)
+SCRIPTS=$(dirname $SELF)
+BASE=$(dirname $SCRIPTS)
+
+function mb {
+    OUTPUT=${BASE}/data/${1}.ppm
+    shift
+    sh -x -c "/usr/bin/time ${BASE}/bin/mandelbrox $* > $OUTPUT"
+    echo ''
+    echo saved output to $OUTPUT
+}
+
+EOF
+
+printf "# x_ratio = %lf; x_pop = %lf\n", $x_ratio, $x_pop;
+printf "# y_ratio = %lf; y_pop = %lf\n", $y_ratio, $y_pop;
+
+sub PrintCommandLine {
+    printf "mb %07d -B plain -x %lf -X %lf -y %lf -Y %lf -M 1000 -w 1024 -h 1024\n", @_;
+}
+
+PrintCommandLine $frame, $x1_min, $x1_max, $y1_min, $y1_max;
+
+while( ++$frame <= $frames ) {
+
+    $x_min_delta /= $x_pop;
+    $x_max_delta /= $x_pop;
+
+    $y_min_delta /= $y_pop;
+    $y_max_delta /= $y_pop;
+
+    $x_min = $x2_min + $x_min_delta;
+    $x_max = $x2_max + $x_max_delta;
+    $y_min = $y2_min + $y_min_delta;
+    $y_max = $y2_max + $y_max_delta;
+
+    PrintCommandLine $frame, $x_min, $x_max, $y_min, $y_max;
+}
+
+PrintCommandLine $frame, $x2_min, $x2_max, $y2_min, $y2_max;
